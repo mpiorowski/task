ed008913f89b73a005a164f7896d7f5d175a1364
James <jamesstroughton@gmail.com>
2023-04-26 13:22:59 +0100

    Style updates + center prop

--- src/routes/AnimeComponent.svelte
+++ src/routes/AnimeComponent.svelte
@@ -4,14 +4,24 @@
     export let title: string;
     export let mal_id: number;
     export let image: string;
+    export let center: boolean = false;
 </script>
 
-<h1>{title}</h1>
-<img
-    class="hover:opacity-50 hover:cursor-pointer"
-    src={image}
-    alt={title}
-    on:click={() => goto(`/${mal_id}`)}
-    height="200px"
-    width="200px"
-/>
+<div class={`flex flex-col ${center && " items-center justify-center"}`}>
+    <h1 class="text-xl font-bold mb-4">{title}</h1>
+    <div>
+        <img
+            class={`hover:opacity-80 transition-all hover:cursor-pointer rounded-lg w-auto max-h-[300px] object-cover  ${
+                !mal_id && "grayscale"
+            }`}
+            src={image}
+            alt={title}
+            on:click={() => mal_id && goto(`/${mal_id}`)}
+            on:keydown={(e) => {
+                if (e.key === "Enter") {
+                    goto(`/${mal_id}`);
+                }
+            }}
+        />
+    </div>
+</div>
